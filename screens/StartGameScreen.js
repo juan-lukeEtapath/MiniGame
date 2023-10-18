e6561bdbdb6617ff43d82ed78bf8f28fef54fb02
juan-lukeEtapath <juan@etapath.com>
2023-10-18 11:53:29 +0200

    style: Added styling to Input section and background

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -4,7 +4,7 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad" autoCapitalize="none" autoCorrect={false}/>
       <PrimaryButton>Confirm</PrimaryButton>
       <PrimaryButton>Cancel</PrimaryButton>
     </View>
@@ -19,8 +19,19 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: '#5E0acc',
+    backgroundColor: '#042a62',
     borderRadius: 6,
     elevation: 10000,
+  },
+  numberInput: {
+    color:  '#c6c013',
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginVertical: 8,
+    borderBottomWidth: 2,
+    borderBottomColor: '#c6c013',
   },
 });
