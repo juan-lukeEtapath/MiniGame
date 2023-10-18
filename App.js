4fa01cfd85b2faf458507970b19d71dc708da9aa
juan-lukeEtapath <juan@etapath.com>
2023-10-18 14:01:34 +0200

    style: Add image + colorgradient as background

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
+import { LinearGradient} from 'expo-linear-gradient';
 import StartGameScreen from './screens/StartGameScreen';
 
 export default function App() {
   return (
-    <StartGameScreen /> 
+    <LinearGradient colors={['#0f5e80','#c6c013']} style={styles.root}>
+      <ImageBackground 
+        source={require('./assets/nums.jpg')}  
+        resizeMode='cover' 
+        style={styles.root}
+        imageStyle={styles.bgImage}>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-
+  root: {
+    flex: 1,
+    //backgroundColor: '#c6c013',
+  },
+  bgImage: {
+    opacity: 0.15,
+  },
 });
